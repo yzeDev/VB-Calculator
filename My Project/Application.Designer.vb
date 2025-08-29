8847afb74dbc7da97a88f69e9bfa7b1897663655
yzeDev <yzekeilv@gmail.com>
2025-08-29 21:40:24 +0800

    v1.1.2

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VB_Practice.Form1
+            Me.MainForm = Global.VB_Practice.Calculator
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
